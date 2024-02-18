88f62b18c0b8e8addd456fbc0ada1797c977166e
AndressaARodrigues <andressa.rodruigues33@gmail.com>
2024-02-18 15:41:27 -0300

    Criando menu e tela inicial

---
+++ src/app/components/Menu/index.js
@@ -0,0 +1,34 @@
+import styles from './index.module.css';
+export default function Menu() {
+    return (
+        <header className={styles.menu}>
+            <div>
+            <img
+                src="/images/logo.svg"
+                alt="Logo Casa Verde"
+            />
+            </div>
+            <nav>
+                <li>
+                    <a href="/">Como fazer</a>
+                    /
+                </li>
+                <li>
+                    <a href="/">Ofertas</a>
+                    /
+                </li>
+                <li>
+                    <a href="/">Depoimentos</a>
+                    /
+                </li>
+                <li>
+                    <a href="/">Vídeos</a>
+                    /
+                </li>
+                <li>
+                    <a href="/">Meu Carrinho</a>
+                </li>
+            </nav>
+        </header>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import AssinaturaNewsletter from "./components/AssinaturaNewsletter";
+import Menu from "./components/Menu";
 
 export default function Home() {
   return (
     <main>
+      <Menu/>
       <AssinaturaNewsletter/>
     </main>
   );
